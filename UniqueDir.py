d8896c3981e8ec1c1352f26682e27d8bb06b82c8
kurtis <kurtis@us.ibm.com>
2017-09-20 15:23:41 -0500

    Finished version 1

--- UniqueDir.py
+++ UniqueDir.py
@@ -2,7 +2,7 @@
 
 import os
 from RunCmdPy import RunCmd
-from Cython.Utility.CConvert import length
+# from Cython.Utility.CConvert import length
 
 class UniqueDir:
 
@@ -19,6 +19,7 @@ class UniqueDir:
 		Verify the directory exists AND user has r/w access.
 		"""
 		rval = 0
+		msg = "*unknown error"
 		if os.path.exists(dir_path):
 			if os.access(dir_path, os.W_OK):
 				rval = 0
@@ -39,20 +40,21 @@ class UniqueDir:
 		first_dir = last_dir = ""
 		
 		# Determine number of potential EXISTING directories
-		cmd = 'ls -1 {}/{}-*|wc -l'.format(dir_path, pattern)
+		cmd = 'find {} -maxdepth 1 -type d -name  "{}*" 2>/dev/null|wc -l'.format(dir_path, pattern)
 		cmd_runner.run(cmd)
+		
 		if cmd_runner.rc == 0:
 			cnt = int(cmd_runner.get_stdout[0])
-			print('>>> INFO: There are {} directories wiht same pattern'.format(cnt))
+			print('>>> INFO: There are {} directories with same pattern'.format(cnt))
 		else:
 			cnt = 0
 		
 		if cnt >0:
 			
 			# There are EXISTING directories.  Determine first and last.
-			cmd = 'ls -1 {}/{}-*|sort|head -n 1'
-			cmd_runner(cmd)
-			if (cmd_runner.get_rc == 0):
+			cmd = 'find {} -maxdepth 1 -type d -name  "{}*" 2>/dev/null|sort|head -n 1'.format(dir_path, pattern)
+			cmd_runner.run(cmd)
+			if cmd_runner.get_rc == 0:
 				first_dir = cmd_runner.get_stdout[0]
 				print('>>> INFO: <first_dir={}>'.format(first_dir))
 			else:
@@ -63,11 +65,11 @@ class UniqueDir:
 				return cnt, first_dir, last_dir
 		
 			# Found first directory, find the last directory
-			cmd = 'ls -1 {}/{}-*|sort|tail -n 1'
-			cmd_runner(cmd)
+			cmd = 'find {} -maxdepth 1 -type d -name  "{}*" 2>/dev/null|sort|tail -n 1'.format(dir_path, pattern)
+			cmd_runner.run(cmd)
 			if (cmd_runner.get_rc == 0):
 				last_dir = cmd_runner.get_stdout[0]
-				print('>>> INFO: <first_dir={}>'.format(first_dir))		
+				print('>>> INFO: <last_dir={}>'.format(last_dir))		
 			else:
 				print('>>> ERROR: Cannot determine last directory.  stderr follows:\n')
 				cmd_runner.dump_stderr()
@@ -77,7 +79,7 @@ class UniqueDir:
 			
 		return cnt, first_dir, last_dir
 		
-	def mkdir(self, dir_path = "/tmp", pattern = '__udo', max_dirs = 5):
+	def mkdir(self, dir_path = "/tmp", pattern = 'dir-', max_dirs = 5):
 		
 		cmd_runner = RunCmd()
 		
@@ -93,27 +95,30 @@ class UniqueDir:
 			print('>>> ERROR: {}'.format(msg))
 			return 1,""
 		
-		# Determine number of directories with patthen, first and last directories
+		# Determine number of directories with pattern, first and last directories
 		dir_cnt, first_dir, last_dir = self.__find_first_last_dir(dir_path, pattern)
+		n = 0
 		if dir_cnt == 0:
 			
-			# There are no existing directories.
-			new_dir = '{}/{}.001'.format(dir_path, pattern)
+			# There are no existing directories. 
+			n = 1
 		elif dir_cnt == -1:
 			return 1, ""
 		else:
 			
 			# Got first at least 1 existing directory.
-			plen = length(dir_path + pattern + '-')
+			plen = len(dir_path + pattern + '-')
 			n = int(last_dir[plen:]) + 1
-			new_dir = '{}/{}.{num:02d}'.format(dir_path, pattern, num=n)
-		
+
+		new_dir = '{}/{}{num:03d}'.format(dir_path, pattern, num=n)
 		cmd = 'mkdir {}'.format(new_dir)
 		cmd_runner.run(cmd)
 		if (cmd_runner.get_rc != 0):
-				print('>>> ERROR: Creation ofr directory <new_dir={}> failed. stderr follows:'.format(new_dir))
-				cmd_runner.dump_stderr()
-				return 2, ''
+			print('>>> ERROR: Creation of directory <new_dir={}> failed. stderr follows:'.format(new_dir))
+			cmd_runner.dump_stderr()
+			return 2, ''
+		else:
+			print('>>> INFO: Created new directory <new_dir={}>'.format(new_dir))
 			
 		# New directory created.
 		self.new_dir = new_dir
@@ -123,28 +128,28 @@ class UniqueDir:
 		if dir_cnt >= max_dirs:
 				
 				# Need to delete the first directory
-				cmd = 'rm -f {}'.format(*last_dir)
+				cmd = 'rm -rf {}'.format(first_dir)
 				cmd_runner.run(cmd)
 				if cmd_runner.get_rc != 0:
 					
 					# Could not delete first dir.
 					print('>>> ERROR: Could not delete directory <first_dir={}>'.format(first_dir))
 					return 4, ""
+				else:
+					print('>>> INFO: Deleted OLDEST directory {}'.format(first_dir))
 			
 		return 0, new_dir		
 			
-		
-		
 def test1():
 	print("Running test1()")
 	t = UniqueDir()
-	rc, new_dir = t.made_dir()
+	rc, new_dir = t.mkdir('/tmp', "kwr540-")
 	if rc == 0:
 		print('>>> INFO: Created new dir <new_dir={}>'.format(new_dir))
 		return 0
-	else
+	else:
 		print(">>> ERROR: Failed making new directory.")
-		retun 1
+		return 1
 		
 def test2():
 	print("Running test2()")
